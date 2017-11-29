a8519dd10aff51d59ac3d16e206342d31b261461
User <User@HT402-ST58>
2017-11-29 10:51:01 +0800

    201711291050

--- MyJava/src/Brad02.java
+++ MyJava/src/Brad02.java
@@ -1,9 +1,33 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Brad02 extends JFrame {
+	private JButton b1, b2, b3;
+	private MyDrawer myDrawer;
+	
 	public Brad02(){
 		// super()
 		super("µøµ¡µ{¦¡");
+		setLayout(new BorderLayout());
+		
+		b1 = new JButton("b1");
+		b2 = new JButton("b2");
+		b3 = new JButton("b3");
+		
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(b1);top.add(b2);top.add(b3);
+		
+		add(top, BorderLayout.NORTH);
+		
+		myDrawer = new MyDrawer();
+		add(myDrawer, BorderLayout.CENTER);
+		
 		
 		setSize(640, 480);
 		setVisible(true);
@@ -14,3 +38,9 @@ public class Brad02 extends JFrame {
 	}
 
 }
+
+class MyDrawer extends JPanel {
+	
+}
+
+
