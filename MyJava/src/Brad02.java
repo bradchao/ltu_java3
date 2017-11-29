7104e42748579e366fefc306457222460c41d39d
User <User@HT402-ST58>
2017-11-29 10:21:49 +0800

    201711291021

---
+++ MyJava/src/Brad02.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Brad02 extends JFrame {
+	public Brad02(){
+		// super()
+		super("µøµ¡µ{¦¡");
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		new Brad02();
+	}
+
+}
